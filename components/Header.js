ff522de8643c8662f12a39313fd8498bee37421b
Ido Barnea <barbareshet@gmail.com>
2021-09-08 14:58:54 +0300

    Added JSConfig.json & aliases paths

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../styles/Heder.module.css";
+import styles from "@/styles/Heder.module.css";
 
 function Header(props) {
     return (
--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout";
+import Layout from "@/components/Layout";
 function About() {
     return (
         <Layout title="DJ Events App - About page">
