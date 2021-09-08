e28ee4d3f66cd6872c1550ff5eca199b82e54490
Ido Barnea <barbareshet@gmail.com>
2021-09-08 14:40:09 +0300

    Header & Footer added to Layout

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from "../styles/Footer.module.css"
+function Footer() {
+
+    return (
+        <footer className={styles.footer}>
+            <p>&copy; DJ Events 2021</p>
+            <p>
+                <Link href="/about">About</Link>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
