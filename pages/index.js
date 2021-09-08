ab1c4abaad7604773a776c5bb79849b606cfaa25
Ido Barnea <barbareshet@gmail.com>
2021-09-08 15:43:12 +0300

    fetching events from API (static data)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function Home({events}) {
+    // will display in the client side - FE rendering
+    // console.log(events);
   return (
     <Layout title="DJ Events App - Home page">
         <div>
@@ -8,4 +11,17 @@ export default function Home() {
         </div>
     </Layout>
   )
+}
+
+export async function getServerSideProps(){
+    const res = await fetch(`${ API_URL }/api/events`)
+    const events = await res.json()
+    // Will display in the terminal - server side rendering
+    // console.log(events);
+    return {
+        props:{
+            events
+        },
+        revalidate: 1 // will run if data is changed, to fetch new events
+    }
 }
